ccfcb38048d17e1d30b9c8fa2e033d6f0bd6aba2
Krista Singleton <kris.singlet231@gmail.com>
2020-09-24 20:02:45 -0700

    Took signIn off home.jsx to rewrite

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -2,20 +2,9 @@ import React from 'react';
 import classes from '../css/styles.css';
 
 const HomePage = () => (
-  <div className={classes.homePage}>
   <div className={classes.homeTitle}>
     <h4>WELCOME TO</h4>
     <h1>LET'S TALK</h1>
-    <h4>Please Sign In</h4>
-    <div>
-      <form>
-        <input placeholder="Username"></input>
-        <input placeholder="Password"></input>
-      </form>
-    </div>
-    <h4>Or if you're new click here to sign up!</h4>
-    <button type="button">Sign Me Up!</button>
-  </div>
     <div><img className="teacup" src="https://lets-talk-environment.s3-us-west-1.amazonaws.com/logos/teaCup.png" alt="teacup" /></div>
   </div>
 );

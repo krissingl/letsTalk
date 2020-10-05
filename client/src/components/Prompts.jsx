203ce39c8bb18c59f1ff5798c64b969a97a4a605
Krista Singleton <kris.singlet231@gmail.com>
2020-10-04 19:35:17 -0700

    Tried to get the prompts array to exist in local scope. Works this way for first prompt, but none therafter

--- client/src/components/Prompts.jsx
+++ client/src/components/Prompts.jsx
@@ -3,30 +3,35 @@ import classes from '../css/styles.css';
 import thoughtPrompts from '../../dist/thoughtPrompts.json';
 
 const PromptBox = ({ prompts, handlePromptsChange }) => {
-  console.log(thoughtPrompts.promptTypes);
-  let pType;
-  // Need to get the values in array form
+  let pArray;
+
   const promptTypesList = thoughtPrompts.promptTypes.map((promptType) => (
-    <option value={promptType.prompts}>{promptType.type}</option>
+    <option key={promptType.key} value={promptType.key}>{promptType.type}</option>
   ));
-  const getThoughtPrompt = ({ promptsArray }) => {
+  const getThoughtPrompt = (promptsArray) => {
+    console.log(promptsArray);
     const { length } = promptsArray;
     const randomIndex = Math.floor(Math.random() * length);
     return promptsArray[randomIndex];
   };
-  const findType = (e) => {
-    console.log(e.target.value);
+  const findTypePrompts = (e) => {
+    const type = e.target.value;
+    for (let i = 0; i < thoughtPrompts.promptTypes.length; i++) {
+      if (thoughtPrompts.promptTypes[i][type]) {
+        pArray = thoughtPrompts.promptTypes[i][type];
+      }
+    }
   };
   return (
     <div className={classes.promptBox}>
       <h5>Don't Know What to Talk About? Get a prompt here!</h5>
       <div>
         <span>Select Prompt Type:</span>
-        <select onChange={findType}>
+        <select onChange={findTypePrompts}>
           {promptTypesList}
         </select>
       </div>
-      <button type="button" className={classes.promptBtn} onClick={() => { const thoughtPrompt = getThoughtPrompt(); handlePromptsChange(thoughtPrompt); }}>Prompt Please!</button>
+      <button type="button" className={classes.promptBtn} onClick={() => { const thoughtPrompt = getThoughtPrompt(pArray); handlePromptsChange(thoughtPrompt); }}>Prompt Please!</button>
       <div>
         {prompts}
       </div>
